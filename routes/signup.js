74a5212ed92b4ceffc788a638a5a5c3444f8152c
meloneusk <80442763+meloneusk@users.noreply.github.com>
2025-06-28 20:33:22 -0400

    s

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const  User  = require('../models/user');
+const  User  = require('../models/User');
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 
